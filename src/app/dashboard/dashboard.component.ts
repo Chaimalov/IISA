855ed3ce4340953d418983c0910aacb6e0ed0caf
Chaim Malov <chaimalov@gmail.com>
2025-09-29 11:18:49 +0300

    fix: layout

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,25 +1,17 @@
-import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { ApplicantsStore } from '../applicants.store';
+import { AgeChartDirective, DateChartDirective } from './charts';
 import { MapComponent } from './map/map.component';
 import { ApplicantsTableComponent } from './table/applicants-table.component';
-import { AgeChartDirective, DateChartDirective } from './charts';
-import { RouterLink } from '@angular/router';
-import { ApplicantComponent } from '../applicant/applicant.component';
 
-const className = 'grid h-full overflow-auto pt-16';
+const className = 'grid h-full overflow-auto pt-20 p-4 gap-4';
 
 @Component({
   selector: 'iisa-dashboard',
   templateUrl: './dashboard.component.html',
-  imports: [
-    ApplicantsTableComponent,
-    AgeChartDirective,
-    MapComponent,
-    ApplicantComponent,
-    DateChartDirective,
-    RouterLink,
-  ],
+  imports: [ApplicantsTableComponent, AgeChartDirective, MapComponent, DateChartDirective, RouterLink],
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [provideCharts(withDefaultRegisterables())],
   host: {
