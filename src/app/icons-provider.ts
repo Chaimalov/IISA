e56a5ccd32ae9960101a8e91464afbaa71d41d48
Chaim Malov <chaimalov@gmail.com>
2025-10-05 13:07:29 +0300

    feet: Display error message on avatar upload failure

--- src/app/icons-provider.ts
+++ src/app/icons-provider.ts
@@ -1,11 +1,11 @@
 import { EnvironmentProviders, importProvidersFrom, inject, provideAppInitializer } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
-import { LucideAngularModule, Moon, Plus, Sun, SunMoon } from 'lucide-angular';
+import { LucideAngularModule, Moon, Plus, Sun, SunMoon, TriangleAlert } from 'lucide-angular';
 
 export function provideIcons(): EnvironmentProviders[] {
   return [
-    importProvidersFrom(LucideAngularModule.pick({ Moon, Sun, Plus, SunMoon })),
+    importProvidersFrom(LucideAngularModule.pick({ Moon, Sun, Plus, SunMoon, TriangleAlert })),
     provideAppInitializer(() => {
       const iconRegistry = inject(MatIconRegistry);
       const sanitizer = inject(DomSanitizer);
